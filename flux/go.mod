c77a1aeb5969700bf5a39e845c280aed5ffd4508
Martin Palma <m@palma.bz>
2022-04-09 12:55:16 +0200

    flux: update test

--- flux/go.mod
+++ flux/go.mod
@@ -2,4 +2,4 @@ module github.com/palmamartin/tmp/flux
 
 go 1.15
 
-require github.com/influxdata/influxdb-client-go/v2 v2.2.1 // indirect
+require github.com/influxdata/influxdb-client-go/v2 v2.8.1
--- flux/main.go
+++ flux/main.go
@@ -6,40 +6,58 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/domain"
 )
 
-const q = `
-from(bucket:"lter_dqc")
-	|>range(start: 0)
-	|>filter(fn: (r) => 
-		r._measurement == "yellow_std" and 
-		r._field == "yellow_std" and
-		r.snipeit_location_ref == "34"
-	)
-	|>last()
-`
+var DefaultTimeFormat domain.DialectDateTimeFormat = "2006-01-02 15:04:05"
 
 func main() {
-	client := influxdb2.NewClient("http://localhost:8086", "")
+	var (
+		url   = flag.String("u", "", "InfluxDB server url")
+		token = flag.String("t", "", "Authentication token. For InfluxDB 1.8 use username:password")
+	)
+	flag.Parse()
+
+	client := influxdb2.NewClient(*url, *token)
+	defer client.Close()
 	queryAPI := client.QueryAPI("")
 
-	result, err := queryAPI.Query(context.Background(), q)
+	const flux = `
+from(bucket:"lter")
+	|>range(start: -4h)
+	|>filter(fn: (r) => 
+		r._measurement == "air_t_avg" and
+		r._field == "air_t_avg" and
+		r.snipeit_location_ref == "34" 
+		
+	)
+	`
+
+	d := influxdb2.DefaultDialect()
+	d.DateTimeFormat = &DefaultTimeFormat
+
+	// dt := &domain.Dialect{
+	// 	DateTimeFormat: &DefaultTimeFormat,
+	// }
+
+	result, err := queryAPI.QueryRaw(context.Background(), flux, d)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(result)
 
-	for result.Next() {
-		r := result.Record()
-		fmt.Printf("%s -- %s -- %s\n", r.Field(), r.Value(), r.Time())
-	}
+	// for result.Next() {
+	// 	r := result.Record()
+	// 	fmt.Printf("%s -- %s -- %s\n", r.Field(), r.Value(), r.Time())
+	// }
 
-	if result.Err() != nil {
-		log.Fatal(err)
-	}
+	// if result.Err() != nil {
+	// 	log.Fatal(err)
+	// }
 
-	client.Close()
 }
