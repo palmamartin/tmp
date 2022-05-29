dff4cb3aed863ac27492a930b2854f0660f8e541
Martin Palma <m@palma.bz>
2022-05-29 21:30:43 +0200

    hotwired: testing stimulus etc...

---
+++ hotwired/src/controllers/hello_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["name"];
+
+    greet() {
+        console.log("you clicked ", this.name)
+    }
+
+    get name() {
+        return this.nameTarget.value
+    }
+    connect() {
+        console.log("hello", this.element)
+    }
+}
